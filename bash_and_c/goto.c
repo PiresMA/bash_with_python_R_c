110562933453388b56cfe0e73f18c3516d0b2182
PiresMA <pires.ma.fisica@gmail.com>
2021-09-11 22:08:23 -0300

    Create goto.c

---
+++ bash_and_c/goto.c
@@ -0,0 +1,23 @@
+// goto.c
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for ( i = 0; i < 5; i++ )
+    {
+        printf( "Outer loop executing. i = %d\n", i );
+        for ( j = 0; j < 2; j++ )
+        {
+            printf( " Inner loop executing. j = %d\n", j );
+            if ( i == 3 )
+                goto stop;
+        }
+    }
+
+    /* This message does not print: */
+    printf( "Loop exited. i = %d\n", i );
+
+    stop: printf( "----------------------> Jumped to stop. i = %d\n", i );
+}
